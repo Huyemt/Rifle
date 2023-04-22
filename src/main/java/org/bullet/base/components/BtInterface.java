ce5bf246e2aef6f301bbb5705fbf0c6735c23594
Huyemt <778600096@qq.com>
2023-04-23 01:20:02 +0800

    Fixed a bug in the for loop and supported the use of "_" identifiers

--- src/main/java/org/bullet/base/components/BtInterface.java
+++ src/main/java/org/bullet/base/components/BtInterface.java
@@ -1,5 +1,6 @@
 package org.bullet.base.components;
 
+import org.bullet.compiler.ast.nodes.BlockNode;
 import org.bullet.compiler.ast.nodes.ProvideNode;
 import org.bullet.exceptions.RuntimeException;
 import org.bullet.interpreter.Interpreter;
